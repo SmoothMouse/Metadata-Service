653036fa09f7ed0a46973da9c4fb3cc1f66950ac
Eugene / Dae <dae@cyberic.eu>
2014-01-07 21:14:25 +0400

    Update settings

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 CACHE_DIR = os.path.join(BASE_DIR, 'cache')
 
 ICONS_DIR = os.path.join(BASE_DIR, 'icons')
-ICONS_URL = 'http://192.168.0.110:8000/icons/'
+ICONS_URL = 'http://metadata.smoothmouse.com/icons/'
 
 USB_IDS_DB_PATH = os.path.join(CACHE_DIR, 'usb-ids.sql')
 
@@ -15,7 +15,3 @@ logging.basicConfig(
 	format='%(asctime)s %(levelname)s %(message)s',
 	datefmt='%H:%M:%S',
 )
-
-# Debug
-#import tempfile
-#tempfile.tempdir = os.path.join(BASE_DIR, 'temp')
